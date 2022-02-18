b012012943e33d4f2a150b066c24b88e6f134d1b
givensuman <givensuman@gmail.com>
2022-02-18 12:13:48 -0500

    Update useParams

--- src/useParams/useParams.ts
+++ src/useParams/useParams.ts
@@ -3,14 +3,27 @@ import { useState, useEffect } from "react"
 type State = object | null
 
 const useParams = (
-    url?: string
+    url: URL['href'] = window.location.href
 ) => {
     const [ params, setParams ] = useState<State>(null)
 
     useEffect(() => {
-        let query: string = url ? new URL(url).search : window.location.search
+        let query: string =  new URL(url).search
         const URLparams = new URLSearchParams(query)
-        setParams(Object.fromEntries(URLparams))
+
+        for (const key of URLparams.keys()) {
+            if (URLparams.getAll(key).length > 1) {
+                setParams({
+                    ...params,
+                    [key]: URLparams.getAll(key)
+                })
+            } else {
+                setParams({
+                    ...params,
+                    [key]: URLparams.get(key)
+                })
+            }
+        }
     }, [url])
     
     return params
